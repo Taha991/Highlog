bc1b44881a14759e1dfce065d0f5f85e9b26647e
Taha991 <tahamohamedman@gmail.com>
2022-10-13 08:36:54 +0200

    adding style for popular container

--- src/sections/MostPopular/MostPopular.js
+++ src/sections/MostPopular/MostPopular.js
@@ -1,8 +1,31 @@
 import './MostPopular.css';
+import Fortnite from '../../assets/images/game-01.jpg'
 
 const MostPopular = () => {
   return(
     <>
+    <div className="section-wrapper">
+      <div className="section-header">
+        <h4>Most Popular</h4>
+      </div>
+      <div className='most-popular-items'>
+        <div className='popular-item'>
+          <div className='card-wrapper'>
+            <img className='popular-img' src={Fortnite} />
+            <div className='popular-content'>
+              <h4 className='popular-title'>
+                Fortnite <br/> 
+                <span>Sandbox</span>
+              </h4>
+              <ul>
+                <li><span>4.8</span></li>
+                <li><span>2.3M</span></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
     </>
   )
 }
