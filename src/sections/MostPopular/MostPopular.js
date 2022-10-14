b14f833e60095f263bc353ed0db4bbcff78851a7
Taha991 <tahamohamedman@gmail.com>
2022-10-14 07:06:01 +0200

    Creating Section Header

--- src/sections/MostPopular/MostPopular.js
+++ src/sections/MostPopular/MostPopular.js
@@ -1,33 +1,34 @@
-import './MostPopular.css';
-import Card from '../Card/Card';
-import data from '../../data';
+import "./MostPopular.css";
+import {Card , SectionHeader} from "../../components/index";
+import data from "../../data";
+
 
 const MostPopular = () => {
-
-  const cards = data.map(item => {
-    return(
+  const cards = data.map((item) => {
+    return (
       <Card
-      key={item.id}
-      coverImg={item.coverImg}
-      title={item.title}
-      info={item.info}
-      rate={item.rate}
-      view={item.view}
+        key={item.id}
+        coverImg={item.coverImg}
+        title={item.title}
+        info={item.info}
+        rate={item.rate}
+        view={item.view}
       />
-    )
-  })
-  return(
+    );
+  });
+  return (
     <>
-    <div className="section-wrapper">
-      <div className="section-header">
-        <h4>Most Popular &#8594; <span style={{"color" : "var(--color-primary)"}}>Right Now</span></h4>
+      <div className="section-wrapper">
+        <SectionHeader>Most Popular &#8594;{" "}
+            <span style={{ color: "var(--color-primary)" }}>Right Now</span></SectionHeader>
+        <div className="most-popular-items">
+          {cards}
+        </div>
       </div>
-      <div className='most-popular-items'>
-      {cards}
-      </div>
-    </div>
+
     </>
-  )
-}
+  );
+};
 
 export default MostPopular;
+
