d65c258bad59aae597f70ab5301739277ad169b5
Taha991 <tahamohamedman@gmail.com>
2022-10-14 08:53:16 +0200

    Creating footer

--- src/sections/Footer/Footer.js
+++ src/sections/Footer/Footer.js
@@ -1,10 +1,18 @@
 import './Footer.css';
+import {FaInstagram} from 'react-icons/fa'
   
 const Footer = () => {
   return(
-    <>
-    
-    </>
+    <footer>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-lg-12'>
+            <p>Copyright © <a href='#'>Nossel Gaming</a> All rights reserved.
+            <br/> Design: <a href='https://www.instagram.com/taha.mohamedt/'>Taha  <FaInstagram/></a></p>
+          </div>
+        </div>
+      </div>
+    </footer>
   )
 }
 export default Footer;
